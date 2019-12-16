d10317256aba181df997a2a45c15c54fe22bd3a1
BenebSiny <stu995106@hotmail.com.tw>
2019-12-16 17:00:38 +0800

    過濾功能

--- src/web/script.js
+++ src/web/script.js
@@ -1,7 +1,15 @@
+if(localStorage.getItem('keyword')!==null){
+    var keyword = localStorage.getItem('keyword');
+    if(keyword==='') keyword = null;
+}
+
 $.getJSON('../news.json', (data)=>{
-    for(let i=0; i<10; i++){
-        $(`#img-${i}`).attr('src', data[i]['image']);
-        $(`#title-${i}`).text(data[i]['title']);
+    for(let i=0, k=0; i<10; i++){
+        if(!data[i]['title'].includes(keyword)){
+            $(`#img-${k}`).attr({'src': data[i]['image'], 'title': data[i]['title']});
+            $(`#title-${k}`).attr({'title': data[i]['title']}).text(data[i]['title']);
+            k++;
+        }
     }
 })
 
@@ -13,4 +21,14 @@ $('img').on('click', function () {
 $('h4').on('click', function (){
     const id = $(this).attr('id').match(/title-(.*)/)[1]
     location.href = `/${id}`
+})
+
+$('.save-keyword-btn').on('click', ()=>{
+    localStorage.setItem('keyword', $('#keyword-name').val());
+    $('#keyword-modal').modal('hide');
+    location.reload();
+})
+
+$('.filter-btn').on('click', ()=>{
+    $('#keyword-name').val(localStorage.getItem('keyword'));
 })
