d3a1a9e91925a4c9f1b35528bc2b001d430cd7c5
林則穎 <dragon95070@gmail.com>
2019-12-26 14:26:31 +0800

    新聞內容抓取

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Koa = require('koa');
 const Router = require('@koa/router');
 const static = require('koa-static');
-const hbs = require('koa-hbs');
 // const {getJSON} = require('jquery');
 const {createReadStream, readFile} = require('fs')
 
@@ -12,9 +11,6 @@ const host = '127.0.0.1';
 const port = 3000;
 
 app.use(static('./src/web'))
-app.use(hbs.middleware({
-    viewPath: `${__dirname}\\src\\web`
-}))
 
 router.get('/', (ctx, next)=>{
     ctx.type = 'html'
@@ -23,33 +19,6 @@ router.get('/', (ctx, next)=>{
 
 router.get('/:id', async(ctx, next)=>{
     const {id} = ctx.params;
-    // var news_title, news_detail_title, news_detail_img, news_source, news_time;
-    // var news;
-    // readFile('./src/web/news.json', "utf-8", (err, data)=>{
-    //     // console.log(data);
-    //     news = data;
-    // });
-
-    // var jsonn;
-    // let interval = setInterval(()=>{
-    //     if(news!=undefined){
-    //         jsonn = JSON.parse(news)
-    //         console.log(jsonn);
-    //         clearInterval(interval);
-    //     }
-    // }, 1);
-    
-    // for(let i=0; i<7; i++)
-    //     console.log(parsedData[i]['title']);
-    
-    // getJSON('./src/web/news.json')
-    // getJSON('./src/web/news.json', (data)=>{
-    // new_title = data[0]['title'];
-    // new_detail_title = data[0]['content'];
-    // new_detail_img = data[0]['image'];
-    // new_source = data[0]['source'];
-    // new_time = data[0]['time']; 
-    // })
 
     ctx.type = 'html'
     ctx.body = createReadStream('./src/web/news_detail.html');
--- src/web/detail_script.js
+++ src/web/detail_script.js
@@ -1,11 +1,16 @@
 $.getJSON('../news.json', (data)=>{
-    $(`#detail_title`).text(data[0]['title']);
-    $(`#detail_title-1`).text(data[0]['content']);
-    $(`#detail_img`).attr('src',data[0]['image']);
-    $(`#source`).text(data[0]['source']);
-    $(`#time`).text(data[0]['time']);
+    let url = location.href;
+    let id = url.match(/http:\/\/127\.0\.0\.1:3000\/(.+)$/)[1]
+    
+    $(`#detail_title`).text(data[id]['title']);
+    $(`#detail_title-1`).text(data[id]['content']);
+    $(`#detail_img`).attr('src',data[id]['image']);
+    $(`#source`).text(data[id]['source']);
+    $(`#time`).text(data[id]['time']);
 })
 
 $('#report').on('click', function () {
     location.href = 'https://cabletvweb.ncc.gov.tw/SWSFront35/SWSF/SWSF01015.aspx'
 })
+
+
